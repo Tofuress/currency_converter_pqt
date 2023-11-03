5b0512fb44a476a09f30b17ae0a5dca8ecfacb4e
Tofuress <99559064+Tofuress@users.noreply.github.com>
2023-11-03 17:34:13 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -14,18 +14,51 @@ class CurrencyConv(QtWidgets.QMainWindow):
         self.init_UI()
 
     def init_UI(self):
+        self.setWindowIcon(QIcon('logo.png'))
         self.setWindowTitle('Конвертер валют')
         self.ui.input_amount.setPlaceholderText('Введите сумму')
         self.ui.output_amount.setPlaceholderText('Результат')
+        self.ui.input_text_currency.setPlaceholderText('Введите аббревиатуру валюты')
+        self.ui.output_text_currency.setPlaceholderText('Введите аббревиатуру валюты')
         self.ui.pushButton.clicked.connect(self.converter)
+    
 
     def converter(self):
         c = CurrencyConverter()
-        input_currency = self.ui.input_currency.currentText()
-        output_currency = self.ui.output_currency.currentText()
-        input_amount = int(self.ui.input_amount.text())
-        output_amount = round(c.convert(input_amount, '%s' % (input_currency), '%s' % (output_currency)), 2)
-        self.ui.output_amount.setText(str(output_amount))
+        input_currency = 'o'
+        output_currency = 'o'
+        self.ui.output_amount.setText('')
+
+        if self.ui.input_currency.currentText() != 'Найдите валюту в списке' and self.ui.input_text_currency.toPlainText() == '':
+            input_currency = self.ui.input_currency.currentText()[0:3]
+        elif self.ui.input_currency.currentText() == 'Найдите валюту в списке' and self.ui.input_text_currency.toPlainText() != '':
+            if self.ui.input_text_currency.toPlainText().upper() in c.currencies:
+                input_currency = self.ui.input_text_currency.toPlainText()
+            else:
+                self.ui.input_text_currency.setText('')
+                self.statusBar().showMessage('Неправильный формат ввода аббревиатуры, попробуйте снова')
+        elif self.ui.input_currency.currentText() == 'Найдите валюту в списке' and self.ui.input_text_currency.toPlainText() == '':
+            self.statusBar().showMessage('Выберите валюту в списке или введите аббревиатуру валюты')
+        else:
+            self.statusBar().showMessage('Выберите валюту в списке ИЛИ введите аббревиатуру валюты')
+        
+        if self.ui.output_currency.currentText() != 'Найдите валюту в списке' and self.ui.output_text_currency.toPlainText() == '':
+            output_currency = self.ui.output_currency.currentText()[0:3]
+        elif self.ui.output_currency.currentText() == 'Найдите валюту в списке' and self.ui.output_text_currency.toPlainText() != '':
+            if is_eng(self.ui.output_text_currency.toPlainText()):
+                output_currency = self.ui.output_text_currency.toPlainText()
+        elif self.ui.output_currency.currentText() == 'Найдите валюту в списке' and self.ui.output_text_currency.toPlainText() == '':
+            self.statusBar().showMessage('Выберите валюту в списке или введите аббревиатуру валюты')
+        else:
+            self.statusBar().showMessage('Выберите валюту в списке ИЛИ введите аббревиатуру валюты')
+        
+        if input_currency != 'o' and output_currency != 'o':
+            input_amount = int(self.ui.input_amount.text())
+            output_amount = round(c.convert(input_amount, '%s' % (input_currency.upper()), '%s' % (output_currency.upper())), 2)
+            if '.' in str(output_amount) and str(output_amount)[str(output_amount).find('.') + 1] == '0':
+                self.ui.output_amount.setText(str(output_amount)[:-2])
+            else:
+                self.ui.output_amount.setText(str(output_amount))
 
 
 app = QtWidgets.QApplication([])
@@ -33,3 +66,4 @@ application = CurrencyConv()
 application.show()
 
 sys.exit(app.exec())
+#pyuic5.exe design.ui -o ui.py
